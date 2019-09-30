1be287e742bb012f8e2c47abadd010df2e39f1ba
munnukumar <50713536+munnukumar@users.noreply.github.com>
2019-10-01 05:14:57 +0530

    Create home.php

---
+++ loan/home.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+    <head><style>
+            :root{
+                --bg-color: url("loan5.jpg");
+                --form-bg-color: rgba(0,0,0,0.6);
+                --border-bg-color: white;
+            }
+            *, *::before, *::after{
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+            html, body{
+                font: 400 15px/1.9 "Lato", sans-serif;
+                background: var(--bg-color);
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                min-height: 100vh;
+                flex-direction: column;
+                text-align: center;
+            }
+            form, .wrapper{
+                border-radius: 10px;
+                background: var(--form-bg-color);
+                box-shadow: 0 0 40px 4px var(--border-bg-color);
+                min-width: 50vw;
+                padding: 5vmin;
+                color: var(--border-bg-color);
+            }
+            a, input[type=submit]{
+                display: block;
+                border: 1px solid var(--form-bg-color);
+                margin: 1vmin 0;
+                text-decoration: none;
+                border-radius: 10px;
+                background: var(--border-bg-color);
+                color: black;
+                padding: 1vmin;
+            }
+            a:hover, input[type=submit]:hover{
+                color: var(--border-bg-color);
+                background: var(--form-bg-color);
+                box-shadow: 0 0 10px 1px var(--form-bg-color);
+            }
+            h1, h2{
+                font: 250 15px/1 "Lato", sans-serif;
+                font-size: 40px;
+            }
+            h2{
+                font-size: 25px;
+            }
+            input, select{
+                min-width: 100%;
+                border: 1px solid var(--border-bg-color);
+            }
+            form{
+                font-size: 20px;
+            }
+        </style>
+    </head>
+    <body>
+        <form method = "post">
+            <p>Loan Amount</p>
+            <input type = "range" min = "0" max = "500000000" value = "250000000" name = "principle"/>
+            <p>Interest Rate(%)</p>
+            <input type = "range" min = "8" max = "15" value = "11.5" step = "1" name = "rate"/>
+            <p>Loan Term(Years)</p>
+            <input type = "range" min = "1" max = "30" value = "16" name = "time"/>
+            <input type = "submit" name = "submit"/>
+            <a href = "index.html">Main Menu</a>
+        </form>
+        <?php
+            if(isset($_POST["principle"])){
+                    echo "<br>Loan Amount : ".$_POST["principle"];
+                    echo "<br>Interest Rate : ".$_POST['rate'];
+                    echo "<br>Loan Years : ".$_POST['time'];
+                    echo "<br>Loan Amount : " . ($_POST["principle"]*$_POST["rate"]*$_POST["time"])/($_POST['time']*12*100*12);
+            } 
+            else{
+                echo "Please slide the Slider Bar and Press Submit.";
+            }
+        ?>
+    </body>
+</html>
